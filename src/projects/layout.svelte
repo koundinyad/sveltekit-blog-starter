7f06cf6bd870a3a02d2618bec8528222cb342fba
aprlc26 <75934187+aprlc26@users.noreply.github.com>
2024-02-02 00:49:11 +0000

    borders

--- src/projects/layout.svelte
+++ src/projects/layout.svelte
@@ -8,10 +8,10 @@
   
     let images = [];
     let filteredImages = [];
-    // const imageModules = import.meta.glob("./images/**/*.{jpg,jpeg,png,gif}"); 
-    const imageModules = import.meta.glob("/static/images/**/*.{jpg,jpeg,png,gif}", { eager: true }); 
+    // const imageModules = import.meta.glob("./images/**/*.{jpg,jpeg,png,gif,webp}");
+    const imageModules = import.meta.glob("/static/images/**/*.{jpg,jpeg,png,gif,webp}", { eager: true });
 
-    console.log(imageModules);
+    // console.log(imageModules);
 
     const loadImageData = async (modulePath) => {
       const imageName = modulePath.split('/').pop().split('.')[0]; // Extract the filename without extension
@@ -24,7 +24,7 @@
       .then((loadedImages) => {
         images = loadedImages;
         filteredImages = images.filter((img) => img.src.includes(slug));
-        console.log(filteredImages);
+        // console.log(filteredImages);
       })
       .catch((error) => {
         console.error('Error loading images:', error);
@@ -35,15 +35,24 @@
         // $image = src;
         // $image = src.replace(/^\./, '');
         $image = src.replace(/^\/static/, '');
-        console.log($image);
+        // console.log($image);
+    }
+
+    function shouldApplyLeftBorder(index) {
+      return index % 3 == 0; // Apply border-l to first item in row
+    }
+
+    function shouldApplyTopBorder(index) {
+      return index < 3; // Apply border-t to items in the first row
     }
   </script>
 
-<div class="grid grid-cols-3 border border-black divide-x divide-black ">
+<div class="grid grid-cols-3 ">
     {#if filteredImages.length > 0}
-      {#each filteredImages as { src, alt }}
-        <div class="h-[50px] md:h-[100px] flex items-center hover:bg-pink font-sans" on:mouseenter={() => displayImage(src)} on:click={() => displayImage(src)}>
-            <p class="m-auto text-center">{alt}</p>
+      {#each filteredImages as { src, alt }, index}
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <div class="h-[50px] md:h-[100px] flex items-center hover:bg-pink font-sans border-r border-b border-black {shouldApplyLeftBorder(index) ? 'border-l' : ''} {shouldApplyTopBorder(index) ? 'border-t' : ''}" on:mouseenter={() => displayImage(src)} on:click={() => displayImage(src)}>
+            <p class="m-auto text-center text-sm">{alt}</p>
         </div>
       {/each}
     {:else}
--- src/projects/more-love.md
+++ src/projects/more-love.md
@@ -6,4 +6,8 @@ slug: more-love
 layout: ./layout.svelte
 ---
 
-hihihih
+A small, loving corner on the internet. Translates gestures into virtual love messages.
+
+Originally a 2-channel interactive installation, consolidated into a web piece.
+
+Made with love with P5.js & Teachable Machine.
