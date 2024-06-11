7203c7febd44f3e6a8f22429164aa7aad6e857e7
aprlc26 <75934187+aprlc26@users.noreply.github.com>
2024-06-11 16:33:27 +0100

    add carousel

---
+++ src/lib/components/Carousel.svelte
@@ -0,0 +1,157 @@
+<script>
+	import Carousel from 'svelte-light-carousel';
+	// import { slide } from 'svelte/types/runtime/transition';
+
+    export let images;
+
+    const slides = images;
+</script>
+
+
+<Carousel {slides}>
+	<!-- <div slot="slide" let:slide>{slide.title}</div> -->
+    <img src={slide.src.replace(/^\/static/, '')} alt={slide.alt} slot="slide" let:slide class="object-cover w-full h-[400px]">
+
+    <button
+        slot="next"
+        let:next
+        let:canScrollNext
+        class={`btn w-fit absolute right-0 top-0 bottom-0 my-auto ${
+            !canScrollNext ? 'opacity-50 cursor-not-allowed' : ''
+        }`}
+        on:click={next}
+        let:a11y
+        {...a11y}
+    >
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+        >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+        </svg>
+    </button>
+    <button
+        slot="prev"
+        let:prev
+        let:canScrollPrev
+        on:click={prev}
+        class={`btn w-fit absolute left-0 top-0 bottom-0 my-auto ${
+            !canScrollPrev ? 'opacity-50 !cursor-not-allowed' : ''
+        }`}
+        let:a11y
+        {...a11y}
+    >
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+        >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+        </svg>
+    </button>
+</Carousel>
+
+
+
+<!-- <script>
+    import { onMount } from 'svelte';
+    import EmblaCarousel from 'embla-carousel';
+    import { onDestroy } from "svelte";
+
+    export let images;
+
+    let embla;
+    let canScrollPrev = false;
+    let canScrollNext = false;
+
+    function checkScroll() {
+        if (embla) {
+            canScrollPrev = embla.canScrollPrev();
+            canScrollNext = embla.canScrollNext();
+        }
+    }
+
+    function prev() {
+        if (embla) embla.scrollPrev();
+    }
+
+    function next() {
+        if (embla) embla.scrollNext();
+    }
+
+    onMount(() => {
+        const options = { loop: true }; // Adjust options as needed
+        embla = EmblaCarousel(document.querySelector('.embla'), options);
+        embla.on('select', checkScroll);
+        checkScroll();
+    });
+</script>
+
+<div class="embla overflow-hidden">
+    <div class="embla_container flex flex-start">
+        {#if images.length > 0}
+            {#each images as { src, alt }}
+                <div class="embla__slide flex-none w-full h-[420px]">
+                    <img class="object-cover w-full h-[420px]" src={src.replace(/^\/static/, '')} {alt} />
+                </div>
+            {/each}
+        {:else}
+            <p>No images to display</p>
+        {/if}
+    </div>
+</div>
+
+<div class="controls">
+    <button on:click={prev} disabled={!canScrollPrev}>Left</button>
+    <button on:click={next} disabled={!canScrollNext}>Right</button>
+</div> -->
+
+
+<!-- <script>
+  import { onMount } from "svelte";
+  import EmblaCarousel from "embla-carousel";
+  import { onDestroy } from "svelte";
+
+  let embla;
+
+  function toLeft() {
+    if (embla && embla.canScrollPrev()) {
+      embla.scrollPrev();
+    }
+  }
+
+  function toRight() {
+    if (embla && embla.canScrollNext()) {
+      embla.scrollNext();
+    }
+  }
+
+  onMount(() => {
+    const options = { loop: true }; // Customize options as needed
+    embla = EmblaCarousel(document.querySelector(".embla"), options);
+
+    // Ensure to cleanup when component is destroyed
+    onDestroy(() => {
+      if (embla) {
+        embla.destroy();
+        embla = null;
+      }
+    });
+  });
+</script>
+
+<div class="embla overflow-hidden">
+  <div class="embla__container flex flex-start">
+    <slot></slot>
+  </div>
+</div>
+
+<button on:click={toLeft}>scroll to the left</button>
+<button on:click={toRight}>scroll to the right</button> -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <!-- HOME -->
 <script>
 	import { writable } from 'svelte/store';
-	import EmblaCarousel from '$lib/components/EmblaCarousel.svelte';
+	import Carousel from '$lib/components/Carousel.svelte';
 
 	// Initiate array for all project images
 	let images = [];
@@ -9,14 +9,14 @@
 	let filteredImages = [];
 
 	// Array of colors
-	const colors = ['#CAC444', '#ED7BA1', '#ED4005', '#91CBDA', '#C1D200'];
+	const colors = ['#CAC444', '#ED7BA1', '#ED4005', '#91CBDA'];
 
 	// Function to get a random color
 	function getRandomColor() {
 		return colors[Math.floor(Math.random() * colors.length)];
 	}
 
-	  // Function to get the next color in the array
+	// Function to get the next color in the array
 	function getNextColor() {
 		const color = colors[colorIndex];
 		// Increment colorIndex and reset to 0 if exceeds array length
@@ -32,7 +32,9 @@
 	// Get the values of the project modules
 	const projects = Object.values(projectModules);
 	// Import all project images
-	const projectImages = import.meta.glob('/static/images/**/*.{jpg,jpeg,png,gif,webp,webm}', { eager: true });
+	const projectImages = import.meta.glob('/static/images/**/*.{jpg,jpeg,png,gif,webp,webm}', {
+		eager: true
+	});
 
 	// FUNCTION
 	// Takes a module path and returns an image's src and alt text
@@ -48,21 +50,23 @@
 	function getImages(slug) {
 		// For each key in project images, call the loadImageData function
 		// Promise.all([...]) takes the array of promises returned by map and returns a single promise that resolves when all the promises in the array have resolved
-		return Promise.all(Object.keys(projectImages).map(loadImageData))
-		//  When the promise resolves, the then method will be called with the resolved value, which is an array of loaded images
-			.then((loadedImages) => {
-				// Assigns the array of loaded images to a variable named images
-				images = loadedImages;
-				// Filter images to only include images with an src property that includes the slug string
-				filteredImages = images.filter((img) => img.src.includes(slug));
-				// Returns the filteredImages array as the result of the then method's callback
-				return filteredImages;
-			})
-			// Catch method will be called if any of the promises in the chain are rejected
-			.catch((error) => {
-				console.error('Error loading images:', error);
-				throw error;
-			});
+		return (
+			Promise.all(Object.keys(projectImages).map(loadImageData))
+				//  When the promise resolves, the then method will be called with the resolved value, which is an array of loaded images
+				.then((loadedImages) => {
+					// Assigns the array of loaded images to a variable named images
+					images = loadedImages;
+					// Filter images to only include images with an src property that includes the slug string
+					filteredImages = images.filter((img) => img.src.includes(slug));
+					// Returns the filteredImages array as the result of the then method's callback
+					return filteredImages;
+				})
+				// Catch method will be called if any of the promises in the chain are rejected
+				.catch((error) => {
+					console.error('Error loading images:', error);
+					throw error;
+				})
+		);
 	}
 
 	(async () => {
@@ -74,7 +78,10 @@
 				// Filter the images based on the project's slug
 				const filteredImages = images.filter((img) => img.src.includes(p.metadata.slug));
 				// Update the $allProjects array with the new project object
-				$allProjects = [{metadata: p.metadata, default: p.default, images: filteredImages}, ...$allProjects];
+				$allProjects = [
+					{ metadata: p.metadata, default: p.default, images: filteredImages },
+					...$allProjects
+				];
 			} catch (error) {
 				console.error('Error loading images:', error);
 			}
@@ -82,7 +89,6 @@
 		// Log the final $allProjects array
 		// console.log($allProjects);
 	})();
-
 </script>
 
 <!-- FEATURED PROJECTS -->
@@ -91,37 +97,43 @@
 	<h4 class="md:col-start-5 col-span-2 text-gray mb-2 col-start-2">Featured Projects</h4>
 	<!-- project block -->
 	{#each $allProjects as project}
-		{#if project.metadata.featured === "yes"}
+		{#if project.metadata.featured === 'yes'}
 			<!-- project title -->
-			<div class="md:col-start-4 md:col-span-6 col-start-1 col-span-6 ">
+			<div class="md:col-start-4 md:col-span-6 col-start-1 col-span-6">
 				<div class="flex flex-row justify-between items-end">
-					<p class="text-xl mb-0">{project.metadata.title}<sup class="text-gray text-sm">{project.metadata.type} ({project.metadata.year})</sup></p>
+					<p class="text-xl mb-0">
+						{project.metadata.title}<sup class="text-gray text-sm"
+							>{project.metadata.type} ({project.metadata.year})</sup
+						>
+					</p>
 					{#if project.metadata.link}
-						<a href={project.metadata.link} target="_blank" class="text-sm px-2 py-1 rounded text-white" style="background-color: {getRandomColor()};">Visit ↗</a>
-
+						<a
+							href={project.metadata.link}
+							target="_blank"
+							class="text-sm px-2 py-1 rounded text-white"
+							style="background-color: {getRandomColor()};">Visit ↗</a
+						>
 					{/if}
 				</div>
-
 			</div>
 			<!-- image carousel -->
 			<div class="md:col-start-4 md:col-span-6 col-start-1 col-span-6 md:mb-12">
-				<EmblaCarousel>
-					{#if project.images.length > 0}
+				<Carousel images={project.images}>
+					<!-- {#if project.images.length > 0}
 						{#each project.images as { src, alt }}
-							<div class="embla__slide flex-none w-full h-[400px]">
-								<img class="object-cover w-full h-[400px]" src={src.replace(/^\/static/, '')} {alt} />
+							<div class="embla__slide flex-none w-full h-[420px]">
+								<img class="object-cover w-full h-[420px]" src={src.replace(/^\/static/, '')} {alt} />
 							</div>
 						{/each}
 					{:else}
 						<p>No images to display</p>
-					{/if}
-				</EmblaCarousel>
+					{/if} -->
+				</Carousel>
 			</div>
 			<!-- project details -->
 			<div class="md:col-start-10 md:col-span-2 col-start-2 col-span-5 mb-8 md:mb-0">
 				<svelte:component this={project.default} />
 			</div>
-
 		{/if}
 	{/each}
 </section>
@@ -134,17 +146,23 @@
 	<div class="md:col-start-4 md:col-span-3 col-start-1 col-span-3">
 		<div class="flex gap-4">
 			{#each $allProjects as project}
-				{#if project.metadata.featured === "no"}
-				<div class="flex-1">
-					<div class="flex flex-col">
-						<!-- project title -->
-						<p class="mb-2">{project.metadata.title}<sup class="text-gray hidden md:inline-block">{project.metadata.type} ({project.metadata.year})</sup></p>
-						<!-- project details -->
-						<p class="block md:hidden text-gray mb-0">{project.metadata.type} ({project.metadata.year})</p>
-						<!-- project images -->
-						<div class="my-2">
-							<EmblaCarousel>
-								{#if project.images.length > 0}
+				{#if project.metadata.featured === 'no'}
+					<div class="flex-1">
+						<div class="flex flex-col">
+							<!-- project title -->
+							<p class="mb-2">
+								{project.metadata.title}<sup class="text-gray hidden md:inline-block"
+									>{project.metadata.type} ({project.metadata.year})</sup
+								>
+							</p>
+							<!-- project details -->
+							<p class="block md:hidden text-gray mb-0">
+								{project.metadata.type} ({project.metadata.year})
+							</p>
+							<!-- project images -->
+							<div class="my-2">
+								<Carousel images={project.images}>
+									<!-- {#if project.images.length > 0}
 									{#each project.images as { src, alt }}
 										<div class="embla__slide flex-none w-full h-[200px]">
 											<img class="object-cover w-full h-[200px]" src={src.replace(/^\/static/, '')} {alt} />
@@ -152,19 +170,23 @@
 									{/each}
 								{:else}
 									<p>No images to display</p>
-								{/if}
-							</EmblaCarousel>
+								{/if} -->
+								</Carousel>
+							</div>
+							<!-- project details -->
+							<svelte:component this={project.default} />
+							{#if project.metadata.link}
+								<a
+									href={project.metadata.link}
+									target="_blank"
+									class="text-sm px-2 py-1 rounded text-white"
+									style="background-color: {getRandomColor()};">Visit ↗</a
+								>
+							{/if}
 						</div>
-						<!-- project details -->
-						<svelte:component this={project.default} />
-						{#if project.metadata.link}
-							<a href={project.metadata.link} target="_blank" class="text-sm px-2 py-1 rounded text-white" style="background-color: {getRandomColor()};">Visit ↗</a>
-						{/if}
 					</div>
-				</div>
 				{/if}
 			{/each}
 		</div>
 	</div>
-
 </section>
